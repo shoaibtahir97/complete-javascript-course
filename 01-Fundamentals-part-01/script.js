3e3e144716861f156719b1c3c2c4da6815ea4cb3
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2023-03-23 17:02:37 +0500

    covered assignment operator

--- 01-Fundamentals-part-01/script.js
+++ 01-Fundamentals-part-01/script.js
@@ -1,3 +1,23 @@
 let js = 'Awesome';
 
 console.log(js)
+
+//BASIC OPERATORS
+let now = 2023;
+let myAge = now - 1997
+console.log(myAge);
+
+console.log(myAge * 2, myAge / 2, 2 ** 3)
+
+//Concatenation with Plus Sign +
+let firstName = "Shoaib";
+let lastName = "Tahir";
+console.log(firstName + "  " + lastName)
+
+//Assignment Operator = 
+let sum = 10 + 5; // 15
+sum += 10;// sum = sum + 10 => 25
+sum *= 4; // sum = sum * 4 => 100
+sum++; //sum = sum + 1
+sum-- //sum = sum - 1 
+console.log(sum)
