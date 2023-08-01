96b361ec3b653fcf3f66d05dca5fd5d512713f63
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2023-08-01 19:56:42 +0500

    accessed all html elements

---
+++ 07.DOM-Projects/Modal/app.js
@@ -0,0 +1,9 @@
+"use-strict";
+
+const openModalBtn = document.querySelectorAll("show-modal");
+
+const modal = document.querySelector("modal");
+
+const closeModalBtn = document.querySelector("close-modal");
+
+const overlay = document.querySelector("overlay");
